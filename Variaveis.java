f859674d7dafe4cdde7ce4892551eea39848190b
João Netto <jovinettonetto@gmail.com>
2023-02-07 15:16:38 +0000

    save 1.1

---
+++ Variaveis.java
@@ -0,0 +1,22 @@
+public class Variaveis {
+    public static void main(String[] args) {
+
+        int idade = 39;
+
+        char letra = 'M';
+
+        boolean vivo = false;
+
+        byte qdb = 7;
+
+        double salario = 1302.00;
+
+        float salarioMenor = 1212.00f;
+
+        short alunosPalmital = 10000;
+
+        long populacaoPalmital = 22000;
+
+        System.out.println(idade);
+    }
+}
