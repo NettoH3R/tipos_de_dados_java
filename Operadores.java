92eb382093a2bf3da5f20ba4caf7ee265f1d0122
João Netto <jovinettonetto@gmail.com>
2023-02-14 14:43:37 +0000

    save 1.5

--- Operadores.java
+++ Operadores.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importa a classe para coletar os dados do teclado
 
 public class Operadores {
     public static void main(String[] args) {
@@ -13,17 +13,25 @@ public class Operadores {
         // System.out.println(idade);
 
         // idade+=4;
-        Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in/* System.in pega os dados da entrada padrão*/); /* Cria um novo objeto para pegar os dados do teclado, o nome nn pode ser repetido */
+        Scanner entrada2 = new Scanner(System.in);
+        
+        int n1;
+        int n2;
 
-        int idade;
+        System.out.print("Digite o primeiro Número: ");
+        n1 =  entrada2.nextInt();
 
-        System.out.print("Digite Sua Idade: ");
+        System.out.print("Digite o Segundo número Número: ");
+        n2 = entrada.nextInt();
 
-        idade=entrada.nextInt();
+        int soma = n1 + n2;
 
-        System.out.println("sua idade é: " + idade + ".");
+        System.out.println("a Soma dos números é: " + soma);
 
 
+
+        
     }
 
 }
