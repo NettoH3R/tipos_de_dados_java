63f04d99b39c6367616afb0268dc990c68c56d6c
João Netto <jovinettonetto@gmail.com>
2023-02-14 14:28:44 +0000

    save 1.4

---
+++ Operadores.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Operadores {
+    public static void main(String[] args) {
+        // int idade;
+        // idade = 12;
+
+        // idade++;
+
+        // System.out.println(idade);
+        // idade++;
+
+        // System.out.println(idade);
+
+        // idade+=4;
+        Scanner entrada = new Scanner(System.in);
+
+        int idade;
+
+        System.out.print("Digite Sua Idade: ");
+
+        idade=entrada.nextInt();
+
+        System.out.println("sua idade é: " + idade + ".");
+
+
+    }
+
+}
+
+
